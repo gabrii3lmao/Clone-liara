79ab93ead13266b7092c33071b1b2293c266df6d
gabrii3lmao <gabrielmluz56@gmail.com>
2026-06-07 16:25:36 -0300

    testing gh actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
+  site: "https://gabrii3lmao.github.io",
+  base: "/Clone-liara",
+  output: "static",
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
