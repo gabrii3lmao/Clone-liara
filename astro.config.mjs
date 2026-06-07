1cc1228e52bb168f308a2a3c6e658d635c498088
gabrii3lmao <gabrielmluz56@gmail.com>
2026-06-07 16:33:26 -0300

    fixing typos

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://gabrii3lmao.github.io/Clone-liara",
-  base: "/",
+  site: "https://gabrii3lmao.github.io",
+  base: "/Clone-liara",
   output: "static",
 
   vite: {
